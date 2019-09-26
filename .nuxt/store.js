a52096df2c53b999b0f96c4fcd9d34d2e476e2e0
藤井麟太郎 <atarimaetaisou@icloud.com>
2019-09-26 18:30:18 +0900

    create task.ts

--- .nuxt/store.js
+++ .nuxt/store.js
@@ -17,6 +17,7 @@ void (function updateModules() {
   store.modules = store.modules || {}
 
   resolveStoreModules(require('../store/admin.ts'), 'admin.ts')
+  resolveStoreModules(require('../store/task.ts'), 'task.ts')
   resolveStoreModules(require('../store/user.ts'), 'user.ts')
 
   // If the environment supports hot reloading...
@@ -25,6 +26,7 @@ void (function updateModules() {
     // Whenever any Vuex module is updated...
     module.hot.accept([
       '../store/admin.ts',
+      '../store/task.ts',
       '../store/user.ts',
     ], () => {
       // Update `root.modules` with the latest definitions.
