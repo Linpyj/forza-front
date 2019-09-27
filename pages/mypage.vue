2f10ef32d33e56145cf9b6ca76f60ef30fa85200
藤井麟太郎 <atarimaetaisou@icloud.com>
2019-09-27 14:15:48 +0900

    modify design in mypage.vue

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Hi, {{currentUser.user.name}}</h1>
-  
+  <div class="container">
+    <section class="section">
+      <div
+        class="title"
+        style="padding: 0.4em 0.5em; color: #494949;  background: #f4f4f4; border-bottom: solid 3px #d7d7d7;"
+      >
+        Welcome to {{currentUser.user.name}}'s Page !
+      </div>
+      <div class="columns">
+        <div class="column is-6 is-offset-3">
 
+          <div class="box">
+
+            <div class="field">
+              <p class="control">
+                <a
+                  class="button is-primary is-fullwidth"
+                  @click="createTask"
+                >
+                  Todoを追加
+                </a>
+              </p>
+            </div>
+          </div>　
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
-
 
 
 <script lang="ts">
